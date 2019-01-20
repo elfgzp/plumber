348eba4f22552a564c255e1122b59b26f18ebb78
Gzp_ <741424975@qq.com>
2019-01-20 22:43:45 +0800

    [ADD] add demo data

--- commands/init_demo_data/main.go
+++ commands/init_demo_data/main.go
@@ -18,11 +18,17 @@ func main() {
 
 	u1e := "plumber@elfgzp.cn"
 	_ = models.CreateUser("admin", u1e, "123456")
+	u1, _ := models.GetUserByEmail(u1e)
 	user, _ := models.GetUserByEmail(u1e)
 
 	u2e := "demo@elfgzp.cm"
 	_ = models.CreateUser("demo", u2e, "123456")
 
 	_ = models.CreateTeam("plumber", user)
+	plumberTeam, _ := models.GetTeamByName("plumber")
 	_ = models.CreateTeam("plumber-front-end", user)
+
+	_ = models.CreateProject("plumber-project", "Project management tool develop by golang", plumberTeam.ID, u1, false)
+	_ = models.CreateProject("plumber-demo-project", "Project management tool develop by golang (Demo)", plumberTeam.ID, u1, false)
+
 }
