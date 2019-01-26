a01fc9d8a8ed6c1351c72d4beed369b9a439a8d6
Gzp_ <741424975@qq.com>
2019-01-26 13:32:08 +0800

    [ADD] add task_comment

---
+++ controllers/restful/task_comment.go
@@ -0,0 +1,23 @@
+package restful
+
+import "net/http"
+
+func CreateTaskComment(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateTaskComment(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ListTaskComment(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func RetrieveTaskComment(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DestroyTaskComment(w http.ResponseWriter, r *http.Request) {
+
+}
